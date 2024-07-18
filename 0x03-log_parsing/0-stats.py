2fe0c46b0797797fa9a69024eb5a19904b1c1056
Omar-Algassim <omarggg2@gamil.com>
2024-07-18 20:56:43 +0300

    update the code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
-import signal
 import sys
 import re
-import asyncio
+
+pattern = '\d*\.\d*\.\d*\.\d* - \[\d*-\d*-\d* \d*:\d*:\d*\.\d*\] "GET \/projects\/260 HTTP\/1.1" \d* \d*'
+count = 0
+file_size = 0
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 try:
-    pattern = '\d*\.\d*\.\d*\.\d* - \[\d*-\d*-\d* \d*:\d*:\d*\.\d*\] "GET \/projects\/260 HTTP\/1.1" \d* \d*'
-    count = 0
-    file_size = 0
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    code_print = lambda : print(f"{k}: {v}")
     for line in sys.stdin:
-        if re.match(pattern,
-            line):
-            size = re.search('..\d$', line)
+        if re.match(pattern, line):
+            size = re.search('(?:\d+)$', line)
             status_code = re.search(' \d* ', line)
             status_code = int(status_code.group(0))
             codes[status_code] += 1
