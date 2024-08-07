6eebd89d9084c71bdce17640619d09b83f511015
Omar-Algassim <omarggg2@gamil.com>
2024-08-07 20:02:31 +0300

    fetch data and print it in same order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,39 @@
 #!/usr/bin/node
-const request = require('request')
+const request = require('request');
 
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+const endPOint = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
+function getData (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response) => {
+      if (error) {
+        console.error(error);
+        reject(error);
+      } else {
+        resolve(JSON.parse(response.body));
+      }
+    });
+  });
+}
 
-request.get(url, function (error, response) {
-    if (error) {
-    console.error(error);
-    }else {
-        const data = JSON.parse(response.body)["characters"]
-        data.forEach(link => {
-            request.get(link, function(error, response) {
-                if (error) {
-                    console.error(error)
-                }else {
-                    const film_info = JSON.parse(response.body);
-                    console.log(film_info['name'])
-                }
-            })
-            
-        });
+getData(endPOint)
+  .then(data => {
+    const characters = data.characters;
+    const characterPromises = [];
+    for (let i = 0; i < characters.length; i++) {
+      characterPromises.push(getData(characters[i]));
     }
-})
+    Promise.all(characterPromises)
+      .then(characterDataArray => {
+        characterDataArray.forEach(characterData => {
+          console.log(characterData.name);
+        });
+      })
+      .catch(error => {
+        console.error(`Error fetching character data: ${error.message}`);
+      });
+  })
+  .catch(error => {
+    console.error(`Error fetching film data: ${error.message}`);
+  });
