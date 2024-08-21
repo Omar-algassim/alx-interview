28f33e8e17e5f105a391dafa966ca74bcf0bb795
Omar-Algassim <omarggg2@gamil.com>
2024-08-21 16:40:50 +0300

    delete the debug print

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,6 +12,5 @@ def makeChange(coins, total):
         while coins[n] <= total:
             total -= coins[n]
             ansList.append(coins[n])
-        print(f"{total} {coins[n]}")
         n -= 1
     return len(ansList) if total == 0 else -1
