c62b38d79c6c537620e2db0b24990775620288bd
Omar-Algassim <omarggg2@gamil.com>
2024-08-21 16:39:56 +0300

    solution

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""mkaing change module"""
+
+def makeChange(coins, total):
+    """make change function"""
+    if total <= 0:
+        return 0
+    n = len(coins) - 1
+    coins.sort()
+    ansList = []
+    while n >= 0:
+        while coins[n] <= total:
+            total -= coins[n]
+            ansList.append(coins[n])
+        print(f"{total} {coins[n]}")
+        n -= 1
+    return len(ansList) if total == 0 else -1
