353ce83717e2d6cac081180085423840f6380331
Omar-Algassim <omarggg2@gmail.com>
2024-09-19 18:04:27 +0300

    change the logic and increase performance

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,24 +4,12 @@
 
 def makeChange(coins, total):
     """return the minumum coins to make the total"""
-    memo = {}
-
-    def dp(temp_amount):
-        """decide if the amount can be made with the coins"""
-        if temp_amount == 0:
-            return 0
-        if temp_amount < 0:
-            return -1
-        if temp_amount in memo:
-            return memo[temp_amount]
-
-        min_coins = float('inf')
-
-        for coin in coins:
-            result = dp(temp_amount - coin)
-            if result >= 0:
-                min_coins = min(min_coins, result + 1)
-
-        memo[temp_amount] = min_coins if min_coins != float('inf') else -1
-        return memo[temp_amount]
-    return dp(total)
+    n = len(coins) - 1
+    coins.sort()
+    ansList = []
+    while n >= 0:
+        while coins[n] <= total:
+            total -= coins[n]
+            ansList.append(coins[n])
+        n -= 1
+    return len(ansList) if total == 0 else -1
