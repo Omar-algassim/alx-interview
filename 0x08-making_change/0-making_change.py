77ba0f5b02d43c414ce1ce1f23f9c7ff1b101286
Omar-Algassim <omarggg2@gmail.com>
2024-09-19 20:06:47 +0300

    change varible naming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,12 +4,12 @@
 
 def makeChange(coins, total):
     """return the minumum coins to make the total"""
-    n = len(coins) - 1
+    length = len(coins) - 1
     coins.sort()
-    ansList = []
-    while n >= 0:
-        while coins[n] <= total:
-            total -= coins[n]
-            ansList.append(coins[n])
-        n -= 1
-    return len(ansList) if total == 0 else -1
+    ans_list = []
+    while length >= 0:
+        while coins[length] <= total:
+            total -= coins[length]
+            ans_list.append(coins[length])
+        length -= 1
+    return len(ans_list) if total == 0 else -1
