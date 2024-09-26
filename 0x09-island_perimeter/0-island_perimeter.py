83673e31dfcdeb9eb1c1d3a925ababb4144c41f4
Omar-Algassim <omarggg2@gmail.com>
2024-09-26 15:41:27 +0300

    solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""islan perimeter problem"""
+
+def island_perimeter(grid):
+    """return the island perimeter"""
+    islan_perimeter = 0
+    row = len(grid) 
+    col = len(grid[0])
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                islan_perimeter += 4
+                if (i + 1) < row and grid[i + 1][j] == 1:
+                    islan_perimeter -= 1
+                if (i - 1) >= 0 and grid[i - 1][j] == 1:
+                    islan_perimeter -= 1
+                if (j + 1) < col and grid[i][j + 1] == 1:
+                    islan_perimeter -= 1
+                if (j - 1) >= 0 and grid[i][j - 1] == 1:
+                    islan_perimeter -= 1
+    return islan_perimeter
