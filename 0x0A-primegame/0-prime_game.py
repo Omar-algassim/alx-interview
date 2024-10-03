43fcc807c509bca1e6b0b0375d115402ebc0168b
Omar-Algassim <omarggg2@gmail.com>
2024-10-03 18:04:57 +0300

    fix turn bug

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,23 +6,20 @@ def isWinner(x, nums):
     """function decide if Maria or Ben is wins"""
     win = {0: 0, 1: 0}
     prime_numbers = []
-    current_player = 0
     if x < 0 or len(nums) < x or max(nums) < 1: 
         return None
     
     for role in range(x):
+        current_player = 0
         prime = [False, False] + [True for i in range(2, nums[role] + 1)]
         for i in range(2, nums[role] + 1):
             if prime[i] == True:
                 prime_numbers.append(i)
                 for j in range(i * i, nums[role] + 1, i):
                     prime[j] = False
-        i = 0
-        while prime_numbers and i < len(prime_numbers):
-            num = prime_numbers[i]
+        for num in prime_numbers:
             prime_numbers.remove(num)
             current_player = 1 - current_player
-            i += 1
         win[current_player] += 1
     return 'Maria' if win[0] > win[1] else 'Ben' if win[0] < win[1] else None
 
