c08e356c2cde586729aa9d9228272bb581b013bb
Omar-Algassim <omarggg2@gmail.com>
2024-10-03 20:45:21 +0300

    chnge the logic of winner

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,7 +4,7 @@
 
 def isWinner(x, nums):
     """function decide if Maria or Ben is wins"""
-    win = {0: 0, 1: 0}
+    win = {'maria': 0, 'ben': 0}
     prime_numbers = []
     if x < 0 or len(nums) < x or max(nums) < 1: 
         return None
@@ -16,10 +16,10 @@ def isWinner(x, nums):
             if prime[i] == True:
                 prime_numbers.append(i)
                 for j in range(i * i, nums[round] + 1, i):
-                    prime[j] = False
+                    prime[j] = False    
+        if len(prime_numbers) % 2 == 0:
+            win['ben'] += 1
+        else:
+            win['maria'] += 1
 
-        for _ in prime_numbers:
-            current_player = 1 - current_player
-        win[current_player] += 1
-
-    return 'Maria' if win[0] > win[1] else 'Ben' if win[0] < win[1] else None
+    return 'Maria' if win['maria'] > win['ben'] else 'Ben' if win['maria'] < win['ben'] else None
